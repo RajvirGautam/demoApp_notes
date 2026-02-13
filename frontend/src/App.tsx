30845a196bf00f1ad1ab1b2d651d4f9400f22535
khushik17 <khushikabra2005@gmail.com>
2026-02-13 19:00:59 +0600

    Fixed routing and added Vercel config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from "./page/Login.tsx";
 import NotePage from "./page/Note.tsx";
 
 function App() {
-  
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login/>} />
-        <Route path ="/note" element={<NotePage />} />
+        <Route path="/notes" element={<NotePage />} />  {/* /note se /notes kiya */}
       </Routes>
     </Router>
   )
 }
 
-export default App
+export default App;
